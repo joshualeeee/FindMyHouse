6f560c4da73cb434909c2f52d1f252f7e159df67
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-08-12 13:38:14 -1000

    feat: add max price

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import time
 
-# setup WebDriver
-driver = webdriver.Chrome(ChromeDriverManager().install())
-bedrooms = '4-bedrooms'
-url = 'https://www.apartments.com/houses/san-luis-obispo-ca/' + bedrooms
+def get_search_parameters():
+    while True:
+        try:
+            bedrooms = int(input("How many bedrooms are you looking for? (Enter a number: 1, 2, 3, 4, etc.): "))
+            max_price = int(input("What is the maximum price you are willing to pay? (Enter a number): "))
+            if bedrooms > 0 and max_price > 0:
+                return f"{bedrooms}-bedrooms-under-{max_price}"
+            else:
+                print("Please enter valid numbers for bedrooms and max price (both greater than 0).")
+        except ValueError:
+            print("Invalid input. Please enter valid numbers.")
 
-# navigate to Apartments.com
-driver.get(url)
+def run_apartments_scraper():
+    try:
+        # Get user's bedroom preference and max price
+        search_parameters = get_search_parameters()
 
-# Wait for presence of XPath expression
-WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'/html/body/div[1]/main/section/div[1]/section[2]/div[2]')))
+        # setup WebDriver
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        url = f'https://www.apartments.com/houses/san-luis-obispo-ca/{search_parameters}'
 
-# get the HTML source code of the loaded web page
-source = driver.page_source
-soup = BeautifulSoup(source, 'html.parser')
+        # navigate to Apartments.com
+        driver.get(url)
 
-# Extract data using Beautiful Soup:
-# get div elements with the class 'placardContainer'
-parent = soup.find('div', 'placardContainer').find('ul')
-# get all list elements with class "mortar-wrapper"
-listings = parent.find_all('li', "mortar-wrapper")
+        # Wait for presence of XPath expression
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/main/section/div[1]/section[2]/div[2]')))
 
+        # get the HTML source code of the loaded web page
+        source = driver.page_source
+        soup = BeautifulSoup(source, 'html.parser')
 
-print(len(listings) + " houses found.")
-for i in listings:
-    link = i.find("article")
-    print(link.get('data-url'))
+        # Extract data using Beautiful Soup:
+        # get div elements with the class 'placardContainer'
+        parent = soup.find('div', 'placardContainer').find('ul')
+        # get all list elements with class "mortar-wrapper"
+        listings = parent.find_all('li', "mortar-wrapper")
 
+        print(str(len(listings)) + " houses found.")
+        for i in listings:
+            link = i.find("article")
+            print(link.get('data-url'))
+    finally:
+        # Close the browser window when done
+        driver.quit()
+
+if __name__ == "__main__":
+    run_apartments_scraper()
